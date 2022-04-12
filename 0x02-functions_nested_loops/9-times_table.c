9b26cad0be0e0dae9b80cd5c7a50d6b53a3f2079
NatnaelTaddese <natnaeltaddese3@gmail.com>
2022-04-12 22:13:09 +0300

    fix again

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,27 +20,27 @@ while (a <= 9)
 		rep = a * b;
 		if (b == 0)
 		{
-			_putchar('0' + rep);
+			putchar('0' + rep);
 		}
 		else if (rep < 10)
 		{
-			_putchar(' ');
-			_putchar('0' + rep);
+			putchar(' ');
+			putchar('0' + rep);
 		}
 		else
 		{
-			_putchar('0' + rep / 10);
-			_putchar('0' + rep % 10);
+			putchar('0' + rep / 10);
+			putchar('0' + rep % 10);
 		}
 
 		if (b < 9)
 		{
-			_putchar(',');
-			_putchar(' ');
+			putchar(',');
+			putchar(' ');
 		}
 		b++;
 	}
-	_putchar('\n');
+	putchar('\n');
 	a++;
 }
 
